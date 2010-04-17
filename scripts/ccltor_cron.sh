53c9445d9bfd0f703f3f78b3f39ecfcf9c51bb01
Manu <manu@toshiba2005.(none)>
2010-04-17 21:37:20 +0200

    scripts: erratum in ccltor_cron.sh

--- scripts/ccltor_cron.sh
+++ scripts/ccltor_cron.sh
@@ -3,4 +3,4 @@
 BACKUP_SCRIPT=/home/manu/calculinator/ccltor_backup.sh
 BACKUP_LOG_FILE=/home/manu/calculinator/logs/backup.log
 
-su - manu -c "sh $BACKUP_SCRIPT > $BACKUP_LOG_FILE 2>&1"
+su - manu -c "sh $BACKUP_SCRIPT >> $BACKUP_LOG_FILE 2>&1"
