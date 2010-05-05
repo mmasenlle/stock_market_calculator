0fb4f3951da41ade0ef1d3d40bcc66ae392b8e58
Manu <manu@toshiba2005.localdomain>
2010-05-05 23:28:58 +0200

    scripts: trends table creation sql

---
+++ scripts/trends.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE trends (
+	value	varchar(16) REFERENCES feeder_values,
+	date	date,
+	P	double precision,
+	R1	double precision,
+	S1	double precision,
+	R2	double precision,
+	S2	double precision,
+	R3	double precision,
+	S3	double precision,
+	R4	double precision,
+	S4	double precision,
+	PRIMARY KEY (value, date)
+);
