2c2a158d663cc027f8f3605c4ff1e75eeef5fa1d
manu <manu@toshiba2010.(none)>
2010-05-29 17:36:49 +0200

    scripts: added sql script to create wealth tables

---
+++ scripts/wealth.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE wealth (
+    market          text,
+    date            date,
+    cnt_price       integer,
+    cnt_volume      integer,
+    cnt_capital     integer,
+    open_price      double precision,
+    open_volume     double precision,
+    open_capital    double precision,
+    close_price     double precision,
+    close_volume    double precision,
+    close_capital   double precision,
+    min_price       double precision,
+    min_volume      double precision,
+    min_capital     double precision,
+    mean_price      double precision,
+    mean_volume     double precision,
+    mean_capital    double precision,
+    max_price       double precision,
+    max_volume      double precision,
+    max_capital     double precision,
+    std_price       double precision,
+    std_volume      double precision,
+    std_capital     double precision,
+    PRIMARY KEY (market, date)
+);
