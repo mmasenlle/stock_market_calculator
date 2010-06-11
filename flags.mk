e249870f41e91407886ee5600229e04725536f2b
manu <manu@toshiba2010.(none)>
2010-06-11 22:52:02 +0200

    calculus folder integrated in includes

--- flags.mk
+++ flags.mk
@@ -5,7 +5,7 @@ SRC_ROOT ?= .
 INSTALL_DIR ?= ~/calculinator
 
 INCLUDE = -I$(SRC_ROOT)/ccltor -I$(SRC_ROOT)/ccltor/db -I$(SRC_ROOT)/ccltor/ic \
-	  -I$(SRC_ROOT)/ccltor/utils -I/usr/include/libxml2
+	  -I$(SRC_ROOT)/ccltor/utils -I$(SRC_ROOT)/ccltor/calculus -I/usr/include/libxml2
 
 CCLTOR_LIB = $(SRC_ROOT)/ccltor/ccltor.a
 CCLTOR_LD = -lccltor -Wl,-L$(SRC_ROOT)/ccltor
